df355c13d5eb77501f37df76961a6547832cf6c3
DevNakid <abdssamad.nakid@gmail.com>
2026-08-03 13:59:07 +0100

    array functions

--- Array Iteration/index.js
+++ Array Iteration/index.js
@@ -1,6 +1,6 @@
 const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
 const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
-const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+const dessertMenu = ['Cake - 12$', 'Ice Cream - 24$', 'Pudding - 8$', 'Fruit Salad - 10$'];
 const breakfastMenuHTML = breakfastMenu.map((item,index)=>`<p>item ${index+1}: ${item}</p>`).join("");
 // console.log(breakfastMenuHTML);
 document.getElementById("breakfastMenuItems").innerHTML = breakfastMenuHTML;
@@ -10,3 +10,8 @@ mainCourseMenu.forEach((item,index)=>{
     mainCourseMenuHTML+=`<p>Course : ${index+1}: ${item}</p>`;
 });
 document.getElementById("maincourseMenuItems").innerHTML = mainCourseMenuHTML;
+let desserMenuHTML = '';
+dessertMenu.forEach((item,index)=>{
+    desserMenuHTML+=`<p> item ${index+1}: ${item}</p>`;
+})
+document.getElementById("dessertMenuItems").innerHTML = desserMenuHTML;
